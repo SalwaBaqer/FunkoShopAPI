796a30af554ace65a13ca87677b2fc22b09162e5
salwa a <SalwaBaqer>
2020-11-25 17:25:55 +0300

    init db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const funkoRoutes = require("./routes/funkos");
+const db = require("./db/models");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,16 @@ app.use(cors());
 app.use("/funkos", funkoRoutes);
 //Routes
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+const run = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
