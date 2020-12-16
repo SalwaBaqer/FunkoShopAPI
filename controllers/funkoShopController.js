1a8c7b757deee886fe508a306dbe719fa12f2576
user <s.abaqer@hotmail.com>
2020-12-16 20:34:13 +0300

    bg remove

--- controllers/funkoShopController.js
+++ controllers/funkoShopController.js
@@ -1,5 +1,8 @@
 const { FunkoShop, Funko } = require("../db/models");
 
+var request = require("request");
+var fs = require("fs");
+
 //View ListShop
 exports.funkoShopList = async (req, res, next) => {
   try {
@@ -24,9 +27,35 @@ exports.funkoShopList = async (req, res, next) => {
 exports.funkoShopCreate = async (req, res, next) => {
   try {
     if (req.file) {
-      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
-    }
+      request.post(
+        {
+          url: "https://api.remove.bg/v1.0/removebg",
+          formData: {
+            image_file: fs.createReadStream(`./media/${req.file.filename}`),
 
+            size: "auto",
+          },
+          headers: {
+            "X-Api-Key": "rtdFpg96ycmnPWNaqFekfCGx",
+          },
+          encoding: null,
+        },
+        function (error, response, body) {
+          if (error) return console.error("Request failed:", error);
+          if (response.statusCode != 200)
+            return console.error(
+              "Error:",
+              response.statusCode,
+              body.toString("utf8")
+            );
+          fs.writeFileSync(`./media/${req.file.filename}no-bg.png`, body);
+          console.log(response.body);
+        }
+      );
+    }
+    req.body.image = `http://${req.get("host")}/media/${
+      req.file.filename
+    }no-bg.png`;
     const newFunkoShop = await FunkoShop.create(req.body);
     res.status(201).json(newFunkoShop);
   } catch (error) {
